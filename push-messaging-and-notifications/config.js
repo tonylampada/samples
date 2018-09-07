efc88f77daded233146eaec222f6353c2182a7b5
Tony Lâmpada <tonylampada@gmail.com>
2018-09-07 17:34:58 -0300

    configg.js

--- push-messaging-and-notifications/config.js
+++ push-messaging-and-notifications/config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyC9qrkoWRxed4137pBK_A5dwX3OAUMthak'
+  gcmAPIKey: 'AAAArWBD49c:APA91bH1Z8O8sy78eupFCDQCbKJ_6UMQRf-2I1BXnCObTwvJdKwB5u8JqbkHv0YwSiXO27OwJCAq4H1yjtVPekA_jUbhrXsTfQGCKEWLhX3u6uvb73cfgSkV6j3L40VwEkDsMMXoYpwM'
 };
